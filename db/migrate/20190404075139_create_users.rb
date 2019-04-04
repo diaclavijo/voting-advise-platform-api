4a58f8575028c776ecf67202a00cc0c307d4e367
David Diaz Clavijo <david@diazclavijo.com>
2019-04-04 17:50:51 +0200

    Uses UUID identifier

--- db/migrate/20190404075139_create_users.rb
+++ db/migrate/20190404075139_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :sid, null: false
+      t.string :uuid, null: false
       t.integer :age
       t.string :email
       t.string :genre
--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_075607) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "sid", null: false
+    t.string "uuid", null: false
     t.integer "age"
     t.string "email"
     t.string "genre"
